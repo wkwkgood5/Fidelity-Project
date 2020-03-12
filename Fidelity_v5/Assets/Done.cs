92366cd7f320649fed147a984ebfaf4e03dc6e66
Jintoki <jin.lan.cen@gmail.com>
2020-03-11 21:54:54 -0400

    Added instruction set 2 and ending message
    Instruction set 2 will go into the next scene, tested on VR, and ending message ties right back into main menu. now we need the stages to time out after 1 minute

---
+++ Fidelity_v5/Assets/Done.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Done : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.G))
+		{
+			SceneManager.LoadScene(0);
+			print("G key was pressed");
+		};
+	}
+}
